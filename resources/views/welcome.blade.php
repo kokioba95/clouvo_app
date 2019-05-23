0083a77c39ee73f9ceb284b71378b770b3871fc6
KOKIOBA <berkway.jace.001@gmail.com>
2019-05-23 10:33:08 +0800

    make 5/23/2019

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,99 +1,145 @@
 <!doctype html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-
-        <title>Laravel</title>
-
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
-
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+        <title>Clou-Vo</title>
     </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
+    <div class="flex-center position-ref full-height">
+        
+        @if (Route::has('login'))
+            <div class="top-right links">
+                @auth
+                    <a href="{{ url('/home') }}">Home</a>
+                @else
+                    <a href="{{ route('login') }}">Login</a>
 
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif
-
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
+                    @if (Route::has('register'))
+                        <a href="{{ route('register') }}">Register</a>
+                    @endif
+                @endauth
             </div>
+        @endif
         </div>
-    </body>
-</html>
+
+            <body>
+                <div>
+                    <div class="container">
+                            <div class="row">
+                                <div class="col-sm-2" style="background-color:red;">
+                                    <div class="col-sm-12" style="background-color:blue;">
+                                    <br>
+                                    <br>
+                                    <br>
+                                    <br>
+                                    </div>
+                                    <div class="col-sm-12" style="background-color:green;">
+                                    <br>
+                                    <br>
+                                    <br>
+                                    <br>
+                                    </div>
+                                </div>
+
+                                <div class="col-sm-10" style="background-color:blue">
+                                    blue
+                                    <div class="col-sm-12" style="background-color:green">
+                                        <div id="myCarousel" class="carousel slide" data-ride="carousel">
+                                            <ol class="carousel-indicators">
+                                                <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+                                                <li data-target="#myCarousel" data-slide-to="1"></li>
+                                                <li data-target="#myCarousel" data-slide-to="2"></li>
+                                            </ol>
+
+                                            <div class="carousel-inner">
+                                                <div class="item active">
+                                                    <img src="https://www.custodian.com.au/wp-content/uploads/2017/06/blog-banner.png" style="width:1000px; height:350px;" alt="slide" class="img-responsive center-block">
+                                                </div>
+                                                <div class="item">
+                                                    <img src="https://www.custodian.com.au/wp-content/uploads/2015/02/60-Generic-Clean.Still001-1024x576-1024x576.jpg" style="width:1000px; height:350px;" alt="slide" class="img-responsive center-block">
+                                                </div>
+                                                <div class="item">
+                                                    <img src="https://www.custodian.com.au/wp-content/uploads/2015/03/bigstock-Real-estate-investment-in-flat-77407109-1024x1024.jpg" style="width:1000px; height:350px;" alt="slide" class="img-responsive center-block">
+                                                </div>
+                                            </div>
+
+                                            <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+                                                <span class="glyphicon glyphicon-chevron-left"></span>
+                                                <span class="sr-only">Previous</span>
+                                            </a>
+                                            <a class="right carousel-control" href="#myCarousel" data-slide="next">
+                                                <span class="glyphicon glyphicon-chevron-right"></span>
+                                                <span class="sr-only">Next</span>
+                                            </a>
+                                        </div>
+                                        <br>
+                                        <br>
+                                        <br>
+                                        <br>
+                                    </div>
+                                    <div>
+                                    
+                                    </div>
+
+
+                                    <div class="row">
+                                        <div class="col-sm-4" style="background-color:red" >
+                                        <div class="item">
+                                            <img src="https://www.custodian.com.au/wp-content/uploads/2017/06/blog-banner.png" style="width:350px; height:150px;"alt="slide" class="img-responsive center-block">
+                                        </div>
+                                        red
+                                        </div>
+                                        <div class="col-sm-4" style="background-color:blue" >
+                                        <div class="item">
+                                            <img src="https://www.custodian.com.au/wp-content/uploads/2017/06/blog-banner.png" style="width:350px; height:150px;" alt="slide" class="img-responsive center-block">
+                                        </div>
+                                        blue
+                                        </div>
+                                        <div class="col-sm-4" style="background-color:yellow" >
+                                        <div class="item">
+                                            <img src="https://www.custodian.com.au/wp-content/uploads/2017/06/blog-banner.png" style="width:350px; height:150px;" alt="slide" class="img-responsive center-block">
+                                        </div>
+                                        yellow
+                                        </div>
+                                    </div>
+
+                                    <div class="row">
+                                        <div class="col-sm-6" style="background-color:red" >
+                                        <div class="item">
+                                            <img src="https://www.custodian.com.au/wp-content/uploads/2017/06/blog-banner.png" style="width:350px; height:150px;"alt="slide" class="img-responsive center-block">
+                                        </div>
+                                        red
+                                        </div>
+                                        <div class="col-sm-6" style="background-color:blue" >
+                                        <div class="item">
+                                            <img src="https://www.custodian.com.au/wp-content/uploads/2017/06/blog-banner.png" style="width:350px; height:150px;" alt="slide" class="img-responsive center-block">
+                                        </div>
+                                        blue
+                                        </div>
+                                    </div>
+                                    <div class="row">
+                                        <div class="col-sm-6" style="background-color:yellow" >
+                                        <div class="item">
+                                            <img src="https://www.custodian.com.au/wp-content/uploads/2017/06/blog-banner.png" style="width:350px; height:150px;" alt="slide" class="img-responsive center-block">
+                                        </div>
+                                        yellow
+                                        </div>
+                                        <div class="col-sm-6" style="background-color:green" >
+                                        <div class="item">
+                                            <img src="https://www.custodian.com.au/wp-content/uploads/2017/06/blog-banner.png" style="width:350px; height:150px;" alt="slide" class="img-responsive center-block">
+                                        </div>
+                                        green
+                                        </div>
+                                    </div>
+
+                                </div>
+                        </div>
+                    </div>
+                </div>
+
+            </body>
+        </html>
