43c8c5cab2253eab45d403335943254872fd7379
KOKIOBA <berkway.jace.001@gmail.com>
2019-06-10 11:26:11 +0800

    EDIT Code easy to see

--- app/User.php
+++ app/User.php
@@ -1,11 +1,14 @@
 <?php
+
 namespace App;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 class User extends Authenticatable
+
 {
     use Notifiable;
+
     /**
      * The attributes that are mass assignable.
      *
@@ -14,6 +17,7 @@ class User extends Authenticatable
     protected $fillable = [
         'first_name', 'last_name', 'group_name', 'email', 'password',
     ];
+
     /**
      * The attributes that should be hidden for arrays.
      *
@@ -22,6 +26,7 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+    
     /**
      * The attributes that should be cast to native types.
      *
