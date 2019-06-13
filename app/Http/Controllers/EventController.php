ef597b78c3c55b4dd77b7c78bd638befd8aa5461
KOKIOBA <berkway.jace.001@gmail.com>
2019-06-13 11:53:15 +0800

    Modified(2)-eventinfo-2

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -10,12 +10,8 @@ class EventController extends Controller
     {
         return view('event_list');
     }
-<<<<<<< Updated upstream
-}
-=======
     public function info()
     {
         return view('event_info');
     }
 }
->>>>>>> Stashed changes
