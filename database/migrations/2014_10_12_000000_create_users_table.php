ab43ebd80d4dbbd290800ce3ffef88fdf0798678
kokioba95 <49220750+kokioba95@users.noreply.github.com>
2019-06-14 12:54:35 +0800

    registerlogin front-end&back-end (#4)
    * after-2
    
    * #8-3 deleted&modified
    
    * AGAIN #8-3 deleted&modifild
    
    * #8-4 modified-file table
    
    * #8-5 AGAIN modified-file

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,10 +14,10 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->increments('id');   
+            $table->increments('id');          
             $table->string('first_name');
             $table->string('last_name');
-            $table->string('group_name');     
+            $table->string('group_name');         
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
